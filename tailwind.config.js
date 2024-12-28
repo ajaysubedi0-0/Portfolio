1d15a9bea0fc8db36a111ba0318849d1d487d004
ajaysubedi0-0 <AjaySubedi@protonmail.com>
2024-12-28 16:20:28 +0545

    tailwind-integration

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./blog/templates/**/*.html', './protfolio/templates/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
